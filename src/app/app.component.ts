967bb8b2943cca8a836b346c4766f092b8495db5
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-06-25 20:28:32 -0300

    devOps challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devOps';
+
+  total:number=0;
+  personas:number=0;
+  propina:number=0;
+  impuestos:number=0;
+
+  totalPropina:number=0;
+  totalImpuestos:number=0;
+
+  aPagar:number=0;
+
+  calcularPropina(){
+    this.totalPropina = (this.total*this.propina)/100;
+    return this.totalPropina;
+  }
+
+  calcularImpuestos(){
+    this.totalImpuestos = (this.total*this.impuestos)/100;
+    return this.totalImpuestos;
+  }
+
+  calcular(){
+    this.aPagar = this.total + this.totalImpuestos + this.totalPropina;
+    return this.aPagar;
+
+  }
+
 }
